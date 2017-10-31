6759a3e249ac943d06795da2f017d5c9d10b917d
xiong <574362260@qq.com>
2017-10-31 14:18:50 +0800

    apis.py modified

--- eveside vision1.0/firstapp/apis.py
+++ eveside vision1.0/firstapp/apis.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
 from django.contrib.auth.models import User
 from .models import UserProfile, Video, Ticket, Comment, Save, Cmtticket, CommentCmt, Followers, Followings, SiXin
